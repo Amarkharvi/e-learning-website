0209b3f898a98f821945435f8eeb5e311693f51c
Amarkharvi <amarkharvi316@gmail.com>
2020-11-17 20:16:02 +0530

    initial commit

---
+++ courses/api/urls.py
@@ -0,0 +1,19 @@
+from courses.api.views import CourseEnrollView
+from os import name
+from django.urls import path,include
+from rest_framework import routers
+from . import views
+
+app_name='courses_api'
+
+router=routers.DefaultRouter()
+router.register('courses',views.CourseViewSet)
+
+urlpatterns=[
+    path('subjects/',
+    views.SubjectListView.as_view(),name='subject_list'),
+    path('subjects/<pk>/',views.SubjectDetailView.as_view(),
+    name='subject_detail'),
+    path('courses/<pk>/enroll/',views.CourseEnrollView.as_view(),name='course_enroll'),
+    path('',include(router.urls)),
+]
